53893ab579028723818a0f361635b600b1a13591
Thorne <1083621486@qq.com>
2021-07-12 17:50:05 +0800

    feat: 添加amplify模块

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
+// import css
+import '@/assets/css/normalize.css'
+import '@/assets/css/index.scss'
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Amplify from 'aws-amplify'
+import amplifyConfig from '@/constant/amplify-config.json'
+import toast from '@/components/toast/toast.js'
 
 Vue.config.productionTip = false
+
+Vue.prototype.toast = toast
+
+Amplify.configure(amplifyConfig)
+
+console.log(process.env)
 
 new Vue({
   router,
