ac3fbaa6659e6c7093032bd09b65912b20b9cf4b
Thorne <1083621486@qq.com>
2021-07-12 18:13:43 +0800

    添加注册页面

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 // import Home from '../views/Home.vue'
 import Login from '@/views/login/Login.vue'
+import SignUp from '@/views/login/SignUp.vue'
 
 Vue.use(VueRouter)
 
@@ -10,6 +11,11 @@ const routes = [
     path: '/',
     name: 'Login',
     component: Login,
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: SignUp,
   },
   // {
   //   path: "/about",
---
+++ src/views/login/SignUp.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="sign-up">
+    <button @click="signUp">Sign Up</button>
+  </div>
+</template>
+
+<script>
+import api from '@/apis/index.js'
+export default {
+  name: 'SignUp',
+  data() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    async signUp() {
+      this.username = 'zhangp'
+      this.password = '123456'
+      try {
+        const user = await api.signUp(this.username, this.password)
+        console.log(user)
+      } catch (e) {
+        this.toast.show(e.message)
+        console.log(e)
+      }
+    },
+  },
+}
+</script>
