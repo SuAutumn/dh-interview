e3aa8e5a41c8592b40b05b527425be4b1e0cefbb
zhangp <peng1.zhang@goodwe.com>
2024-05-18 11:36:44 +0800

    chore: test commit

---
+++ src/components/layout/left-navs/left-navs.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="left-navs">
+    <div class="left-navs_items">
+      <slot name="items"></slot>
+    </div>
+    <div class="left-navs_content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'left-navs',
+}
+</script>
+
+<style lang="scss" scoped>
+.left-navs {
+  overflow: hidden;
+  height: 100vh;
+
+  &_items {
+    float: left;
+    width: $left-navs-width;
+  }
+
+  &_content {
+    margin-left: $left-navs-width;
+  }
+}
+</style>
